c10a6c7b70448e352c093d5afb736c33bd5deaeb
tresDaniel <tres.daniel@hotmail.com>
2015-11-17 18:15:04 -0300

    Atualização do statistics.js, para tentar mostrar os preços dos itens nesta página. Problemas com o ".getElementsByClassName("oitm")"

--- statistics.js
+++ statistics.js
@@ -1,11 +1,45 @@
-function betHistory(){
+var trigger = 0;
 
-	$('.box-shiny').after('<br /> <div class="statistics">Item name:</div>');
-	var won_itens = document.getElementsByClassName("full")[2].getElementsByClassName("details9968").getElementsByClassName("oitm");
+function betHistory(){
+	if(trigger == 0){
+		$('.box-shiny').after('<br /> <div class="statistics">Item name:</div>');
+		trigger++;
+
+		var placed_itens = document.getElementById("ajaxCont").getElementsByClassName("details9968")[0].getElementsByClassName("oitm");
+		//var won_itens = document.getElementById("ajaxCont").getElementsByClassName("details9968")[1].getElementsByClassName("oitm");
+
+		for (i = 0; i < placed_itens.length; i++) {
+	        var text = placed_itens[i].getElementsByClassName("name")[0].getElementsByTag("b").textContent;
+	        var value = parseFloat(text.replace("%20", "+"));
+	        //won_sum += value;
+	    }
+		alert(text);
+		console.log(placed_itens.length);
+	}
 }
 
+function getPrice(){
+
+	var request = $.ajax({
+	  url: "http://steamcommunity.com/market/priceoverview/?currency=1&appid=570&market_hash_name=Green+Ural",
+	  method: "GET",
+	  data: { id : menuId },
+	  dataType: "json"
+	});
+	 
+	request.done(function( msg ) {
+//  $( "#log" ).html( msg );
+		alert(msg);
+	});
+	 
+	request.fail(function( jqXHR, textStatus ) {
+	  alert( "Request failed: " + textStatus );
+	});
+	}
+
 function main() {
+
+	//Shows Bet History
    	document.getElementsByClassName("button")[5].addEventListener("click", betHistory);
-   
 }
 main();
